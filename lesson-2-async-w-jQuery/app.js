2b86877b2beb9dfb783fa91742fdeb28efa5cb2b
Chris Krezias <chris.krezias@aqurance.com>
2018-04-15 15:08:04 +0300

    lesson 3 completed

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -6,31 +6,29 @@
     let searchedForText;
     const responseContainer = document.querySelector('#response-container');
 
-    function addImage() {
+    function addImage(images) {
         let htmlContent = '';
-        const data = JSON.parse(this.responseText);
 
-        if (data && data.results && data.results[0]) {
-            const firstImage = data.results[0];
+        if (images && images.results && images.results[0]) {
+            const firstImage = images.results[0];
             htmlContent = `<figure>
-        		<img src="${firstImage.urls.regular}" alt="${searchedForText}">
-        		<figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
-        	</figure>`;
+                <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`;
         } else {
             htmlContent = '<div class="error-no-articles">No images available</div>';
         }
         responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
     }
 
-    function addArticles() {
+    function addArticles(articles) {
         let htmlContent = '';
-        const data = JSON.parse(this.responseText);
 
-        if (data.response && data.response.docs && data.response.docs.length > 1) {
-            htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
-        			<h2><a href="${article.web_url}">${article.headline.main}</a></h2>
-        			<p>${article.snippet}</p>
-        		</li>`).join('') + '</ul>';
+        if (articles.response && articles.response.docs && articles.response.docs.length > 1) {
+            htmlContent = '<ul>' + articles.response.docs.map(article => `<li class="article">
+                    <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                    <p>${article.snippet}</p>
+                </li>`).join('') + '</ul>';
         } else {
             htmlContent = '<div class="error-no-articles">No articles available</div>';
         }
@@ -44,18 +42,13 @@
 
         $.ajax({
             url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
-            headers:{
-            	Authorization:'Client-ID c93b7891db01823f875039614a42cc90f924967807252d6b44c3873460c69717'
+            headers: {
+                Authorization: 'Client-ID c93b7891db01823f875039614a42cc90f924967807252d6b44c3873460c69717'
             }
         }).done(addImage);
 
-        const articleRequest = new XMLHttpRequest();
-
-        articleRequest.onload = addArticles;
-        // articleRequest.onerror = function(err) {
-        //     requestError(err, 'articles');
-        // };
-        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=58968c67d937448db2b1b6d8ca8c11f6`);
-        articleRequest.send();
+        $.ajax({
+            url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&&api-key=58968c67d937448db2b1b6d8ca8c11f6`
+        }).done(addArticles);
     });
 })();
--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -1,12 +1,62 @@
-(function () {
+/* eslint-env jquery */
+
+(function() {
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
     let searchedForText;
     const responseContainer = document.querySelector('#response-container');
 
-    form.addEventListener('submit', function (e) {
+    function addImage(data) {
+        let htmlContent = '';
+        const firstImage = data.results[0];
+
+        if (firstImage) {
+            htmlContent = `<figure>
+            <img src="${firstImage.urls.small}" alt="${searchedForText}">
+            <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+        </figure>`;
+        } else {
+            htmlContent = 'Unfortunately, no image was returned for your search.'
+        }
+
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+
+    function addArticles(data) {
+        let htmlContent = '';
+
+        if (data.response && data.response.docs && data.response.docs.length > 1) {
+            htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
+                    <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                    <p>${article.snippet}</p>
+                </li>`).join('') + '</ul>';
+        } else {
+            htmlContent = '<div class="error-no-articles">No articles available</div>';
+        }
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
+
+    function requestError(e, part) {
+        console.log(e);
+        responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">Oh no! There was an error making a request for the ${part}.</p>`);
+    }
+
+    form.addEventListener('submit', function(e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
+                headers: {
+                    Authorization: 'Client-ID c93b7891db01823f875039614a42cc90f924967807252d6b44c3873460c69717'
+                }
+            }).then(response => response.json())
+            .then(addImage)
+            .catch(e => requestError(e, 'image'));
+
+        fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&&api-key=58968c67d937448db2b1b6d8ca8c11f6`)
+            .then(response => response.json())
+            .then(addArticles)
+            .catch(e => requestError(e, 'article'));
     });
 })();
